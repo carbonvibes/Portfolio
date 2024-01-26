a6eb3358a24f957a7615beca3fde19d3581e5627
Arjun Sekar <116697912+carbonvibes@users.noreply.github.com>
2024-01-26 16:37:13 +0530

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -126,10 +126,11 @@ const educationInfo = {
   display: true, // Set false to hide this section, defaults to true
   schools: [
     {
-      schoolName: "Harvard University",
-      logo: require("./assets/images/harvardLogo.png"),
-      subHeader: "Master of Science in Computer Science",
-      duration: "September 2017 - April 2019",
+      schoolName: "Indian Institute of Technology Gandhinagar",
+      logo: require("./assets/images/iitgn.png"),
+      subHeader: "BTech Electrical Engineering",
+      subHeader: "Minor in Computer Science Engineering",
+      duration: "September 2022 - May 2026",
       desc: "Participated in the research of XXX and published 3 papers.",
       descBullets: [
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit",
@@ -138,7 +139,7 @@ const educationInfo = {
     },
     {
       schoolName: "Stanford University",
-      logo: require("./assets/images/stanfordLogo.png"),
+      logo: require("./assets/images/cv.png"),
       subHeader: "Bachelor of Science in Computer Science",
       duration: "September 2013 - April 2017",
       desc: "Ranked top 10% in the program. Took courses about Software Engineering, Web Security, Operating Systems, ...",
